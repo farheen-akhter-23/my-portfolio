17525b954f18a5d6646a3c1c9b33b51d68ba1157
Farheen Akhter <akhter0893@gmail.com>
2025-04-03 12:46:47 -0700

    next.js portfolio

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+const repo = 'my-portfolio'; // replace with your repo name
+
+module.exports = {
+  output: 'export', // required for static export
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
+  trailingSlash: true, // optional, helps with GitHub Pages
+};
