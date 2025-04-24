0399d524a5813561ba539de9bd82d09742e4c1ae
Farheen Akhter <akhter0893@gmail.com>
2025-04-23 23:01:59 -0700

    fix: url updated

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -51,9 +51,8 @@ const AppBanner = () => {
 					className="flex justify-center sm:block"
 				>
 					<a
-						download="Farheen_Akhter_Resume.pdf"
-						href="/files/Farheen_Akhter_Resume.pdf"
-						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+						download="Farheen_Akhter.pdf"
+						href="https://farheen-akhter-23.github.io/my-portfolio/files/Farheen-Akhter.pdf"
 						aria-label="Download Resume"
 					>
 						<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
